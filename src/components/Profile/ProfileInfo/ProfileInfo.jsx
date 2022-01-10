59e102d269e49ec181ccb8234c703ae2888b85fc
MDS <web.masster.vevo@gmail.com>
2022-01-10 22:31:41 +0300

    refactoring fo status with use hooks

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,138 +1,110 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Preloader from "../../common/Preloader/Preloader";
 import style from "./ProfileInfo.module.css";
 import textureBackground from "../../../assets/image/socialBG.png";
 import user_default_avatar from "../../../assets/image/user_default_avatar2.png";
 
-class ProfileInfo extends React.Component {
-  state = {
-    editMode: false,
-    status: this.props.status,
+export const ProfileInfo = (props) => {
+  const [editMode, setEditMode] = useState(false);
+  const [status, setStatus] = useState(props.status);
+
+  const onStatusChange = (e) => {
+    setStatus(e.target.value);
   };
-  onStatusChange = (e) => {
-    this.setState({
-      status: e.target.value,
-    });
+  const activateEditMode = () => {
+    setEditMode(true);
   };
-  activateEditMode = () => {
-    this.setState({
-      editMode: true,
-    });
+  const deactivateEditMode = () => {
+    setEditMode(false);
+    props.updateStatus(status);
   };
-  deactivateEditMode = () => {
-    this.setState({
-      editMode: false,
-    });
-    this.props.updateStatus(this.state.status);
-  };
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.status !== this.props.status) {
-      this.setState({
-        status: this.props.status,
-      });
-    }
-  }
-  render() {
-    if (!this.props.profile) return <Preloader />;
-    return (
-      <div className={style.profileInfo}>
-        <div className={style.wrapperBackground}>
-          <img src={textureBackground} alt="bg" />
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
+
+  if (!props.profile) return <Preloader />;
+
+  return (
+    <div className={style.profileInfo}>
+      <div className={style.wrapperBackground}>
+        <img src={textureBackground} alt="bg" />
+      </div>
+      <div className={style.profileInfoWrapper}>
+        <img
+          className={style.profileInfoAvatar}
+          src={
+            props.profile.photos.small != null
+              ? props.profile.photos.small
+              : user_default_avatar
+          }
+          alt={props.profile.fullName}
+        />
+        <h3 className={style.profileInfoFullName}>{props.profile.fullName}</h3>
+
+        <div className={style.profileInfoAboutMe}>
+          <span className={style.status} onDoubleClick={activateEditMode}>
+            {!editMode && (props.status || "---")}
+            {editMode && (
+              <input
+                className={style.editInputStatus}
+                autoFocus={true}
+                onBlur={deactivateEditMode}
+                type="text"
+                value={status}
+                onChange={onStatusChange}
+              />
+            )}
+          </span>
         </div>
-        <div className={style.profileInfoWrapper}>
-          <img
-            className={style.profileInfoAvatar}
-            src={
-              this.props.profile.photos.small != null
-                ? this.props.profile.photos.small
-                : user_default_avatar
-            }
-            alt={this.props.profile.fullName}
-          />
-          <h3 className={style.profileInfoFullName}>
-            {this.props.profile.fullName}
-          </h3>
 
-          <div className={style.profileInfoAboutMe}>
-            <span
-              className={style.status}
-              onDoubleClick={this.activateEditMode}
-            >
-              {this.state.editMode === false ? (
-                this.props.status || "---"
-              ) : (
-                <input
-                  className={style.editInputStatus}
-                  autoFocus={true}
-                  onBlur={this.deactivateEditMode}
-                  type="text"
-                  value={this.state.status}
-                  onChange={this.onStatusChange}
-                />
-              )}
-            </span>
-          </div>
-
-          <div className={style.profileInfoContacts}>
-            {this.props.profile.contacts.facebook === null ? (
-              ""
-            ) : (
-              <a href={"https://" + this.props.profile.contacts.facebook}>
-                Facebook
-              </a>
-            )}
-            {this.props.profile.contacts.website === null ? (
-              ""
-            ) : (
-              <a href={"https://" + this.props.profile.contacts.website}>
-                Website
-              </a>
-            )}
-            {this.props.profile.contacts.vk === null ? (
-              ""
-            ) : (
-              <a href={"https://" + this.props.profile.contacts.vk}>Vk</a>
-            )}
-            {this.props.profile.contacts.twitter === null ? (
-              ""
-            ) : (
-              <a href={"https://" + this.props.profile.contacts.twitter}>
-                Twitter
-              </a>
-            )}
-            {this.props.profile.contacts.youtube === null ? (
-              ""
-            ) : (
-              <a href={"https://" + this.props.profile.contacts.instagram}>
-                Instagram
-              </a>
-            )}
-            {this.props.profile.contacts.youtube === null ? (
-              ""
-            ) : (
-              <a href={"https://" + this.props.profile.contacts.youtube}>
-                Youtube
-              </a>
-            )}
-            {this.props.profile.contacts.github === null ? (
-              ""
-            ) : (
-              <a href={"https://" + this.props.profile.contacts.github}>
-                Github
-              </a>
-            )}
-            {this.props.profile.contacts.mainLink === null ? (
-              ""
-            ) : (
-              <a href={"https://" + this.props.profile.contacts.mainLink}>
-                MainLink
-              </a>
-            )}
-          </div>
+        <div className={style.profileInfoContacts}>
+          {props.profile.contacts.facebook === null ? (
+            ""
+          ) : (
+            <a href={"https://" + props.profile.contacts.facebook}>Facebook</a>
+          )}
+          {props.profile.contacts.website === null ? (
+            ""
+          ) : (
+            <a href={"https://" + props.profile.contacts.website}>Website</a>
+          )}
+          {props.profile.contacts.vk === null ? (
+            ""
+          ) : (
+            <a href={"https://" + props.profile.contacts.vk}>Vk</a>
+          )}
+          {props.profile.contacts.twitter === null ? (
+            ""
+          ) : (
+            <a href={"https://" + props.profile.contacts.twitter}>Twitter</a>
+          )}
+          {props.profile.contacts.youtube === null ? (
+            ""
+          ) : (
+            <a href={"https://" + props.profile.contacts.instagram}>
+              Instagram
+            </a>
+          )}
+          {props.profile.contacts.youtube === null ? (
+            ""
+          ) : (
+            <a href={"https://" + props.profile.contacts.youtube}>Youtube</a>
+          )}
+          {props.profile.contacts.github === null ? (
+            ""
+          ) : (
+            <a href={"https://" + props.profile.contacts.github}>Github</a>
+          )}
+          {props.profile.contacts.mainLink === null ? (
+            ""
+          ) : (
+            <a href={"https://" + props.profile.contacts.mainLink}>MainLink</a>
+          )}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ProfileInfo;
--- src/redux/user-selectors.js
+++ src/redux/user-selectors.js
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect'
+// import { createSelector } from 'reselect'
 export const getUsers = state => state.usersPage.userList
 export const getPageSize = state => state.usersPage.pageSize
 export const getTotalUsersCount = state => state.usersPage.totalUsersCount
